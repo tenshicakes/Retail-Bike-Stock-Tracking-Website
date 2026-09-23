8c4821f4a961602deb5d5bcf2567cfdf47088f14
tenshicakes <jmontalan8@gmail.com>
2026-09-24 04:07:44 +0800

    removed a commennt

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -19,7 +19,7 @@ class DatabaseSeeder extends Seeder
         $this->call([
             AccountSeeder::class,
             ProductSeeder::class,
-            // You can add ProductSeeder::class here later!
+            
         ]);
     }
 }
